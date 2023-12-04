af7868eb5b23f52e8b4579fdf0ca4ed15fdf16bc
Aiden <118379307+awa03@users.noreply.github.com>
2023-12-04 18:13:00 -0500

    Delete _deps directory

--- _deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
+++
@@ -1,3 +0,0 @@
-_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
-  /Users/aidenallen/Desktop/COP4530/Untitled/spellChecker/_deps/ftxui-src/src/ftxui/dom/flexbox_config.cpp \
-  /Users/aidenallen/Desktop/COP4530/Untitled/spellChecker/_deps/ftxui-src/include/ftxui/dom/flexbox_config.hpp
